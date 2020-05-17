3bbab300d56ecc56d06597a8c1b546580a074ab5
SachinReddy <Sachin.Reddy@target.com>
2020-05-17 13:57:24 -0400

    Renamed filed and navigation between profile and home fragments.

--- app/src/main/java/com/sachinreddy/feature/HomeFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/HomeFragment.kt
@@ -7,19 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onStart() {
@@ -39,7 +39,7 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
         }
     }
 
@@ -50,8 +50,7 @@ class FirstFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home ->
-                Snackbar.make(view!!, "Going to profile...", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
             R.id.action_start_collab ->
                 Snackbar.make(view!!, "Starting a collab...", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
