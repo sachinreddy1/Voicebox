2d013be6384502a3c433d0f963ead8bbba179086
SachinReddy <Sachin.Reddy@target.com>
2020-05-18 09:37:25 -0400

    Animations added. Navigation between Profile and Home fragments. Action bar setup.

--- app/src/main/java/com/sachinreddy/feature/HomeFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/HomeFragment.kt
@@ -2,18 +2,15 @@ package com.sachinreddy.feature
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HomeFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,22 +22,14 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         setHasOptionsMenu(true)
         (requireActivity() as AppCompatActivity).apply {
-            setSupportActionBar(toolbar)
             supportActionBar?.apply {
+                title = getString(R.string.app_name)
                 setDisplayHomeAsUpEnabled(true)
                 setHomeAsUpIndicator(R.drawable.ic_team_member)
                 setHomeActionContentDescription(getString(R.string.open_profile_card))
             }
         }
         super.onStart()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- app/src/main/java/com/sachinreddy/feature/ProfileFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/ProfileFragment.kt
@@ -1,23 +1,35 @@
 package com.sachinreddy.feature
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class ProfileFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        (requireActivity() as AppCompatActivity).apply {
+            supportActionBar?.apply {
+                title = getString(R.string.profile)
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_arrow_back_black)
+                setHomeActionContentDescription(getString(R.string.back_to_home))
+            }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
@@ -27,5 +39,20 @@ class ProfileFragment : Fragment() {
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+            R.id.button_settings ->
+                Snackbar.make(view!!, "Opening settings...", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        return true
     }
 }
