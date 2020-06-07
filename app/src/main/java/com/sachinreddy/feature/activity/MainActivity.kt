b4b0dee893ace4cbe04b4281f6ed13def8bdd972
SachinReddy <Sachin.Reddy@target.com>
2020-06-06 21:47:50 -0400

    Added new login layout and stuff.

--- app/src/main/java/com/sachinreddy/feature/activity/MainActivity.kt
+++ app/src/main/java/com/sachinreddy/feature/activity/MainActivity.kt
@@ -3,31 +3,11 @@ package com.sachinreddy.feature.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sachinreddy.feature.R
-import com.sachinreddy.feature.fragment.SplashFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val splashFragment = SplashFragment()
-        supportFragmentManager.beginTransaction().add(
-            R.id.fragment_main_container,
-            splashFragment, "splashFragment"
-        ).commit()
-        fragment_main_container.animate().apply {
-            duration = 1000
-            alpha(0f)
-            scaleX(1.2f)
-            scaleY(1.2f)
-            withEndAction {
-                alpha(1f)
-                scaleX(1f)
-                scaleY(1f)
-                supportFragmentManager.beginTransaction().remove(splashFragment).commit()
-            }
-        }
     }
 }
