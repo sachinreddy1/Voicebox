77f0dda8749d0f7330a38ee996282c063e84558d
SachinReddy <Sachin.Reddy@target.com>
2020-08-12 20:10:00 -0400

    ye

--- app/src/main/java/com/sachinreddy/feature/fragment/DraftsFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/fragment/DraftsFragment.kt
@@ -1,7 +1,10 @@
 package com.sachinreddy.feature.fragment
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -11,7 +14,6 @@ import androidx.navigation.fragment.findNavController
 import com.sachinreddy.feature.R
 import com.sachinreddy.feature.adapter.SongAdapter
 import com.sachinreddy.feature.auth.Authenticator
-import com.sachinreddy.feature.data.Song
 import com.sachinreddy.feature.injection.appComponent
 import com.sachinreddy.feature.viewModel.AppViewModel
 import kotlinx.android.synthetic.main.activity_app.*
@@ -38,29 +40,10 @@ class DraftsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_main, menu)
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home ->
-                validNavController?.navigate(R.id.action_homeFragment_to_profileFragment)
-            R.id.action_start_collab ->
-                validNavController?.navigate(R.id.action_homeFragment_to_friendsFragment)
-            R.id.action_solo_session -> {
-                Authenticator.apply {
-                    val song = Song(currentUser?.artistId!!, "Untitled", false)
-                    currentUser?.songs?.add(song)
-                    mDatabaseReference
-                        .child(currentUser?.artistId!!)
-                        .setValue(currentUser)
-                        .addOnSuccessListener {
-                            songAdapter.songs = currentUser?.songs!!
-                            songAdapter.notifyDataSetChanged()
-                        }
-                }
-            }
+                validNavController?.navigate(R.id.action_DraftsFragment_to_HomeFragment)
         }
         return true
     }
@@ -93,10 +76,10 @@ class DraftsFragment : Fragment() {
         (requireActivity() as AppCompatActivity).apply {
             setSupportActionBar(app_action_bar)
             supportActionBar?.apply {
-                title = getString(R.string.app_name)
+                title = getString(R.string.profile)
                 setDisplayHomeAsUpEnabled(true)
-                setHomeAsUpIndicator(R.drawable.ic_account_circle_dark)
-                setHomeActionContentDescription(getString(R.string.open_profile_card))
+                setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+                setHomeActionContentDescription(getString(R.string.back_to_home))
             }
         }
     }
--- app/src/main/java/com/sachinreddy/feature/fragment/HomeFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/fragment/HomeFragment.kt
@@ -2,8 +2,10 @@ package com.sachinreddy.feature.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import com.sachinreddy.feature.R
 import com.sachinreddy.feature.adapter.SongAdapter
 import com.sachinreddy.feature.viewModel.AppViewModel
+import kotlinx.android.synthetic.main.activity_app.*
 import javax.inject.Inject
 
 /**
@@ -27,7 +30,33 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_drafts, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupActionBar()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                validNavController?.navigate(R.id.action_HomeFragment_to_ProfileFragment)
+        }
+        return true
+    }
+
+    private fun setupActionBar() {
+        setHasOptionsMenu(true)
+        (requireActivity() as AppCompatActivity).apply {
+            setSupportActionBar(app_action_bar)
+            supportActionBar?.apply {
+                title = getString(R.string.app_name)
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_account_circle_dark)
+                setHomeActionContentDescription(getString(R.string.open_profile_card))
+            }
+        }
+    }
 
     private val validNavController get() = findNavController().takeIf { it.valid }
     private val NavController.valid get() = currentDestination?.id == R.id.HomeFragment
