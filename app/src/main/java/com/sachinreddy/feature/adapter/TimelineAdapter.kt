afa45828453fb2330be7f39bd20f1d7a3869be45
SachinReddy <Sachin.Reddy@target.com>
2020-08-16 18:07:08 -0400

    updated timeline. added barNumber

--- app/src/main/java/com/sachinreddy/feature/adapter/TimelineAdapter.kt
+++ app/src/main/java/com/sachinreddy/feature/adapter/TimelineAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sachinreddy.feature.R
 
@@ -15,13 +16,14 @@ class TimelineAdapter(val context: Context) : RecyclerView.Adapter<RecyclerView.
 
     override fun getItemCount(): Int = Integer.MAX_VALUE
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) =
+        (holder as TimelineAdapter.TimelineViewHolder).setBarNumber(position)
 
-    }
+    internal inner class TimelineViewHolder(timeView: View) : RecyclerView.ViewHolder(timeView) {
+        private val barNumber: TextView = timeView.findViewById(R.id.barNumber)
 
-    internal inner class TimelineViewHolder(songView: View) : RecyclerView.ViewHolder(songView) {
-        fun setSongDetails() {
-
+        fun setBarNumber(position: Int) {
+            barNumber.text = (position + 1).toString()
         }
     }
 }
