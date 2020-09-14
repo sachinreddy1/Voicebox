2b817bd0d68f19c438c1fd5141e6309db0675274
sachinreddy1 <reddysachin2014@gmail.com>
2020-09-13 23:02:42 -0400

    for this life i cannot cahnge

--- app/src/main/java/com/sachinreddy/feature/data/table/Cell.kt
+++ app/src/main/java/com/sachinreddy/feature/data/table/Cell.kt
@@ -1,10 +1,15 @@
 package com.sachinreddy.feature.data.table
 
+import java.nio.ByteBuffer
+import java.util.*
+
 class Cell(
     var isSelected: Boolean = false,
     var isPlaying: Boolean = false,
 
     var columnPosition: Int,
     var rowPosition: Int? = null,
-    var hasData: Boolean = false
+    var hasData: Boolean = false,
+
+    var qArray: Queue<ByteBuffer> = LinkedList<ByteBuffer>()
 )
--- app/src/main/java/com/sachinreddy/feature/fragment/HomeFragment.kt
+++ app/src/main/java/com/sachinreddy/feature/fragment/HomeFragment.kt
@@ -1,14 +1,12 @@
 package com.sachinreddy.feature.fragment
 
-import android.media.MediaPlayer
+import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -26,22 +24,28 @@ import com.sachinreddy.feature.injection.appComponent
 import com.sachinreddy.feature.viewModel.AppViewModel
 import kotlinx.android.synthetic.main.activity_app.*
 import kotlinx.android.synthetic.main.fragment_home.*
-import java.io.File
-import java.io.IOException
-import java.lang.Exception
+import java.nio.ByteBuffer
+import java.util.*
 import javax.inject.Inject
 
 
 const val REQUEST_PERMISSION_CODE = 200
 val PERMISSIONS = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.RECORD_AUDIO)
+const val BufferElements2Rec = 1024 // want to play 2048 (2K) since 2 bytes we use only 1024
+const val BytesPerElement = 2 // 2 bytes in 16bit format
 
 class HomeFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val appViewModel by activityViewModels<AppViewModel> { viewModelFactory }
 
-    var mediaRecorder: MediaRecorder? = null
+    private var audioRecord: AudioRecord? = null
+    private var recorderThread: Thread? = null
     private var isRecording = false
+
+    private val RECORDER_SAMPLERATE = 8000
+    private val RECORDER_CHANNELS: Int = android.media.AudioFormat.CHANNEL_IN_MONO
+    private val RECORDER_AUDIO_ENCODING: Int = android.media.AudioFormat.ENCODING_PCM_16BIT
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -66,7 +70,7 @@ class HomeFragment : Fragment() {
             override fun onRecord() {
                 (adapter.selectedCell as Cell).apply {
                     hasData = true
-                    startRecording(columnPosition, rowPosition ?: 0)
+                    startRecording(this)
                     adapter.notifyDataSetChanged()
                 }
             }
@@ -83,39 +87,56 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
-    private fun startRecording(columnPosition: Int, rowPosition: Int) {
+    private fun startRecording(cell: Cell) {
         if (isRecording) return
 
-        mediaRecorder = MediaRecorder()
-        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        audioRecord = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+            RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement
+        )
 
-        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        val file = File(path, "/${columnPosition}_${rowPosition}.3gp")
-
-        mediaRecorder?.setOutputFile(file)
-
-        try {
-            mediaRecorder?.prepare();
-            mediaRecorder?.start();
-            isRecording = true
-        }
-        catch (e: Exception){
-            e.printStackTrace();
-        }
+        audioRecord?.startRecording()
+        isRecording = true
+        recorderThread = Thread(Runnable {
+            var bData = ByteBuffer.allocate(BufferElements2Rec)
+            val bbArray = ByteArray(bData.remaining())
+            bData.get(bbArray)
+            while (isRecording) {
+                val result = audioRecord?.read(bbArray, 0, BufferElements2Rec)
+                println("READ DATA")
+                if (result != null && result > 0) {
+                    cell.qArray.add(bData)
+                    bData = ByteBuffer.allocate(BufferElements2Rec)
+                } else if (result == AudioRecord.ERROR_INVALID_OPERATION) {
+                    println("Recording: Invalid operation error")
+                    break
+                } else if (result == AudioRecord.ERROR_BAD_VALUE) {
+                    println("Recording: Bad value error")
+                    break
+                } else if (result == AudioRecord.ERROR) {
+                    println("Recording: Unknown error")
+                    break
+                }
+                try {
+                    Thread.sleep(10)
+                } catch (e: InterruptedException) {
+                    break
+                }
+            }
+        }, "AudioRecorder Thread")
+        recorderThread?.start()
     }
 
     private fun stopRecording() {
         if (!isRecording) return
 
-        mediaRecorder?.let {
-            it.stop()
-            it.release()
+        if (audioRecord != null) {
             isRecording = false
+            audioRecord?.stop()
+            audioRecord?.release()
+            audioRecord = null
         }
-
-        mediaRecorder = null
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
