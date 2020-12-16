c1f028f95d43cc12c6d57dd1787434f1651716c7
sachinreddy1 <reddysachin2014@gmail.com>
2020-12-16 00:53:14 -0500

    stuff

--- app/src/main/java/com/sachinreddy/feature/table/ui/view/OperationButton.kt
+++ app/src/main/java/com/sachinreddy/feature/table/ui/view/OperationButton.kt
@@ -3,6 +3,7 @@ package com.sachinreddy.feature.table.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.sachinreddy.feature.R
 
@@ -12,9 +13,16 @@ class OperationButton(context: Context, attributeSet: AttributeSet) :
     init {
         View.inflate(context, R.layout.operation_button, this)
         val buttonCircle: ConstraintLayout = findViewById(R.id.button_circle)
+        val buttonIcon: ImageView = findViewById(R.id.button_icon)
         val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.OperationButton)
         buttonCircle.backgroundTintList =
             attributes.getColorStateList(R.styleable.OperationButton_buttonCircleColor)
+        buttonIcon.setImageResource(
+            attributes.getResourceId(
+                R.styleable.OperationButton_buttonImage,
+                R.drawable.ic_selection
+            )
+        )
         attributes.recycle()
     }
 }
